6464c4df8b279d200248ad3683d2e077a97a4b0e
Nguyen Do Quoc Duy <duy12345616@gmail.com>
2025-12-25 01:24:06 +0700

    Add health and death

--- Assets/Objects/Square/SquareController.cs
+++ Assets/Objects/Square/SquareController.cs
@@ -9,6 +9,8 @@ namespace Objects.Square
     {
         [SerializeField] private GameManager.GameManager gameManager;
         [SerializeField] private GameUI gameUI;
+        [SerializeField] private float remainHealth = 10f;
+        [SerializeField] private SpriteRenderer spriteRenderer;
 
         public void SetGameManager(GameManager.GameManager gm)
         {
@@ -48,10 +50,31 @@ namespace Objects.Square
                         _currentStopTime = Constants.Constants.SquareSpawner.StopTime;
                         gameUI.AddScore(1);
                     }
+
+                    if (remainHealth <= 10f)
+                    {
+                        remainHealth += Time.deltaTime;
+                    }
+
+                    if (remainHealth >= 3f)
+                    {
+                        spriteRenderer.color = new Color(0.26f, 0.7f, 0.33f);
+                    }
                 }
                 else
                 {
                     transform.position = Vector3.MoveTowards(transform.position, _currentTarget.transform.position, Constants.Constants.SquareSpawner.MoveSpeed * Time.deltaTime);
+                    remainHealth -= Time.deltaTime;
+                    
+                    if (remainHealth <= 3f)
+                    {
+                        spriteRenderer.color = Color.darkGoldenRod;
+                    } 
+                    
+                    if (remainHealth <= 0f)
+                    {
+                        Destroy(gameObject);
+                    }
                 }
             }
         }

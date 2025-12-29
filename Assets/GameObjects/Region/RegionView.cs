954c78a71cd4f4390aa6475f0b80f87c8662ed23
duy <duy.nguyen@sun.studio>
2025-12-29 14:42:49 +0700

    Refactor

--- Assets/GameObjects/Region/RegionView.cs
+++ Assets/GameObjects/Region/RegionView.cs
@@ -5,15 +5,19 @@ namespace GameObjects.Region
 {
     public class RegionView : MonoBehaviour
     {
-        private RegionController _regionController;
         private IObjectResolver _objectResolver;
         
-        public RegionController RegionController => _objectResolver.Resolve<RegionController>();
-    
+        public RegionController RegionController { get; private set; }
+
         [Inject]
         public void Construct(IObjectResolver resolver)
         {
             _objectResolver = resolver;
+        }
+
+        private void Start()
+        {
+            RegionController = _objectResolver.Resolve<RegionController>();
         }
     }
 }

7684c7f0eac80f033b97197cf79d486954e5ef4c
duy <duy.nguyen@sun.studio>
2025-12-29 14:00:51 +0700

    Refactor

--- Assets/GameManagers/SceneLifeTimeScope.cs
+++ Assets/GameManagers/SceneLifeTimeScope.cs
@@ -9,12 +9,14 @@ namespace GameManagers
     public class SceneLifeTimeScope: LifetimeScope
     {
         [SerializeField] private RegionController regionController;
+        [SerializeField] private GameUI gameUI;
+        [SerializeField] private GameObject regionCenter;
         
         protected override void Configure(IContainerBuilder builder)
         {
             builder.Register<GameManager>(Lifetime.Singleton);
             builder.RegisterInstance(regionController);
-            builder.RegisterComponentInHierarchy<GameUI>();
+            builder.RegisterInstance(gameUI);
         }
     }
 }
